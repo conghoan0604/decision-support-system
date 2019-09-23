7525012c370ad49d822da316203f0e6517900ff3
dunghuynhminh <huynhminhdung_t61@hus.edu.vn>
2019-09-24 01:44:46 +0700

    Sửa interface và các hàm trong Model

--- src/models/ModelInterface.java
+++ src/models/ModelInterface.java
@@ -1,4 +1,32 @@
 package models;
 
 public interface ModelInterface {
+
+    // Common
+    void generateInterestMatrix();
+
+    // Uncertainty theory
+    int laplaceCriteriaPredict();
+    int bestChoiceInBadCasesPredict();
+    int savageCriteriaPredict();
+    int minimumRankPredict();
+
+    // Risk management
+    int nearFutureRiskPredict();
+    int maximumExpectedProfitRiskPredict();
+    int minimumExpectedRegretRiskPredict();
+
+    // Certainty theory
+    int maximumExpectedProfitPredict();
+
+    // Get
+    int getOriginalPrice();
+    int getSellPrice();
+    int getLossPrice();
+
+    // Set
+    void setOriginalPrice(int number);
+    void setSellPrice(int number);
+    void setLossPrice(int number);
+
 }
