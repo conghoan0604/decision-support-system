f16959ed8b0daed49e6b4987c344301aaf986181
dunghuynhminh <huynhminhdung_t61@hus.edu.vn>
2019-09-26 01:02:24 +0700

    bổ sung nốt các trường hợp lựa chọn + sửa chút Interface

--- src/models/Model.java
+++ src/models/Model.java
@@ -5,10 +5,13 @@ import java.lang.IllegalArgumentException;
 public class Model implements ModelInterface {
     public static final int MIN_PRODUCT = 11;
     public static final int MAX_PRODUCT = 14;
+    public static final int[] N_PRODUCT = {11, 12, 13, 14};
     static final int SIZE = 4;
 
     private int originalPrice, lossPrice, sellPrice;
     private int[][] interestMatrix = new int[SIZE][SIZE];
+    private int[][] regretMatrix = new int[SIZE][SIZE];
+    private double[] probabilities = new double[SIZE];
 
     public Model(){
         // null statement
@@ -60,7 +63,17 @@ public class Model implements ModelInterface {
 
     // common
     @Override
-    public void generateInterestMatrix(){
+    public void loadInput(){
+        generateInterestMatrix();
+        generateRegretMatrix();
+    }
+
+    @Override
+    public void loadProbability(double[] array){
+        probabilities = array;
+    }
+
+    private void generateInterestMatrix(){
         int[][] matrix = new int[SIZE][SIZE];
         int interest;
         for (int i = 0; i < SIZE; i++){
@@ -83,45 +96,145 @@ public class Model implements ModelInterface {
         interestMatrix = matrix;
     }
 
+    private void generateRegretMatrix(){
+        int n;
+        int[][] matrix = new int[SIZE][SIZE];
+
+        for (int i = 0; i < SIZE; i++){
+            n = interestMatrix[i][i];
+            for (int j = 0; j < SZIE; j++){
+                matrix[i][j] = interestMatrix[i][j] - n;
+            }
+        }
+
+        regretMatrix = matrix;
+    }
+
     // uncertain
     @Override
     public int laplaceCriteriaPredict(){
-        return 0;
+        double probability = 1 / SIZE;
+        double[] expect_interest = new double[SIZE];
+
+        for (int i = 0; i < SIZE; i++){
+            expect_interest[i] = probability * interestMatrix[i][0];
+            for (int j = 1; j < SIZE; j++){
+                expect_interest[i] += probability * interestMatrix[i][j]; 
+            }
+        }
+
+        int maxExpect = getMaxValueIndexInDoubleArray(expect_interest);
+        return maxExpect;
     }
 
     @Override
     public int bestChoiceInBadCasesPredict(){
-        return 0;
+        int minInterest;
+        int[] worstCases = new int[SIZE];
+
+        for (int i = 0; i < SIZE; i++){
+            minInterest = interestMatrix[i][0];
+            for (int j = 1; j < SIZE; j++) {
+                if (interestMatrix[i][j] < minInterest) {
+                    minInterest = interestMatrix[i][j];
+                }
+            }
+            worstCases[i] = minInterest;
+        }
+        
+        int bestOutcome = getMaxValueIndexInIntegerArray(worstCases);
+        return bestOutcome;
     }
 
     @Override
     public int savageCriteriaPredict(){
-        return 0;   
+        worstCase = new int[SIZE];
+        int maxRegret;
+
+        for (int i = 0; i < SIZE; i++){
+            maxRegret = regretMatrix[i][i];
+            for (int j = 0; j < SIZE; j++){
+                if (regretMatrix[i][j] > maxRegret){
+                    maxRegret = regretMatrix[i][j];
+                }
+            }
+            worstCase[i] = maxRegret;
+        }
+
+        bestOutcome = getMinValueIndexInIntegerArray(worstCase);
+        return bestOutcome;   
     }
 
     @Override
     public int minimumRankPredict(){
-        return 0;
+        int min, max, rank;
+        int[] rankArray = new int[SIZE];
+
+        for (int i = 0; i < SIZE; i++){
+            min = interestMatrix[i][0];
+            max = interestMatrix[i][0];
+
+            for (int j = 1; j < SIZE; j++){
+                val = interestMatrix[i][j];
+                if (val > max){
+                    max = val;
+                } else {
+                    min = val;
+                }
+            }
+
+            rank = max - min;
+            rankArray[i] = rank;
+        }
+
+        int minRank = getMinValueIndexInIntegerArray(rankArray);
+        return minRank;
     }
 
     // Risk management
     @Override
     public int nearFutureRiskPredict(){
-        return 0;
+        int highestProbability = getMaxValueIndexInDoubleArray(array);
+        return highestProbability;
     }
 
     @Override
     public int maximumExpectedProfitRiskPredict(){
-        return 0;
+        double[] expectInterests = new double[SIZE];
+        double expectInterest;
+
+        for (int i = 0; i < SIZE; i++){
+            expectInterest = 0;
+            for (int j = 0; j < SIZE; j++){
+                expectInterest += probabilities[j] * interestMatrix[i][j]; 
+            }
+            expectInterests[i] = expectInterest;
+        }
+        
+        int bestOutcome = getMaxValueIndexInDoubleArray(expectInterests);
+        return bestOutcome;
     }
 
     @Override
     public int minimumExpectedRegretRiskPredict(){
-        return 0;
+        double[] expectRegrets = new double[SIZE];
+        double expectRegret;
+
+        for (int i = 0; i < SIZE; i++){
+            expectRegret = 0;
+            for (int j = 0; j < SIZE; j++){
+                expectRegret += probabilities[j] * regretMatrix[i][j]; 
+            }
+            expectRegrets[i] = expectRegret;
+        }
+        
+        int bestOutcome = getMinValueIndexInDoubleArray(expectRegrets);
+        return bestOutcome;
     }
 
     // Certainty theory
     public int maximumExpectedProfitPredict(){
+        // ???
         return 0;
     }
 
@@ -159,14 +272,52 @@ public class Model implements ModelInterface {
         return index;
     }
 
-    public void printInterestMatrix(){
-        System.out.println("Interest Matrix = ");
-        for (int i = 0; i < SIZE; i++){
-            for (int j = 0; j < SIZE; j++){
-                System.out.printf("%5d", interestMatrix[i][j]);
+
+    private int getMaxValueIndexInIntegerArray(int[] array){
+        int maxVal = array[0];
+        int index = 0;
+
+        for (int i = 1; i < array.length; i++){
+            if (array[i] > maxVal){
+                maxVal = array[i];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+
+    private int getMinValueIndexInIntegerArray(int[] array){
+        minVal = array[0];
+        int index = 0;
+
+        for (int i = 1; i < array.length; i++){
+            if (array[i] < minVal){
+                minVal = array[i];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+
+    private void printMatrix(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[i].length; j++){
+                System.out.printf("%5d", matrix[i][j]);
             }
             System.out.println();
         }
+    }
+
+    public void printInterestMatrix(){
+        System.out.println("Interest Matrix = ");
+        printMatrix(interestMatrix);
+    }
+
+    public void printRegretMatrix(){
+        System.out.println("Regret Matrix = ");
+        printMatrix(regretMatrix);
     }
 
 
