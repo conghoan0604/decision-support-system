a92a7037f3e3ef5dc3945e9d17342f0995fbf8d6
Cong Hoan Ngo <conghoan.0406@gmail.com>
2019-09-28 20:45:51 +0700

    Init simple controller

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,4 +1,61 @@
 package controllers;
 
+import models.Model;
+import views.View;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.io.IOException;
+
+import javax.swing.JButton;
+
 public class Controller {
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public int getModelOriginalPrice() {
+        return model.getOriginalPrice();
+    }
+
+    public int getModelSellPrice() {
+        return model.getSellPrice();
+    }
+
+    public int getModelLossPrice() {
+        return model.getLossPrice();
+    }
+
+    public void setModelOriginalPrice(int number) {
+        model.setOriginalPrice(number);
+    }
+
+    public void setModelSellPrice(int number) {
+        model.setSellPrice(number);
+    }
+
+    public void setModelLossPrice(int number) {
+        model.setLossPrice(number);
+    }
+
+//    public void initView() {
+//
+//    }
+//
+//        public class btnExitAL implements ActionListener {
+//        public void actionPerformed(ActionEvent e) {
+//            System.exit(0);
+//        }
+//    }
+//
+//    public class btnSetDataAL implements ActionListener {
+//        public void actionPerformed(ActionEvent e) {
+//
+//        }
+//    }
 }
--- src/controllers/ControllerInterface.java
+++ src/controllers/ControllerInterface.java
@@ -1,4 +1,13 @@
 package controllers;
 
 public interface ControllerInterface {
+    // Get data from model
+    int getModelOriginalPrice();
+    int getModelSellPrice();
+    int getModelLossPrice();
+
+    // Set data to model
+    void setModelOriginalPrice(int number);
+    void setModelSellPrice(int number);
+    void setModelLossPrice(int number);
 }
